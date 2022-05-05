d88e522b0b0054404f251bc83acaf68ca43cde07
Cleber Simm <clebersimm@gmail.com>
2022-05-05 19:11:21 -0300

    Section 4: Course React Native - The practical Guide
    https://www.udemy.com/course/react-native-the-practical-guide/

---
+++ components/ui/Card.js
@@ -0,0 +1,31 @@
+import { StyleSheet, View } from "react-native";
+import Colors from "../../constants/colors";
+
+function Card({ children }) {
+
+    return (
+        <View style={styles.inputContainer}>
+            {children}
+        </View>
+    );
+
+}
+
+export default Card;
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 16,
+        marginHorizontal: 24,
+        marginTop: 36,
+        backgroundColor: Colors.primary800,
+        borderRadius: 8,
+        elevation: 4,
+        shadowColor: "black",
+        shadowOpacity: 0.5,
+        shadowOffset: { width: 4, height: 4 },
+        shadowRadius: 6,
+    },
+});
